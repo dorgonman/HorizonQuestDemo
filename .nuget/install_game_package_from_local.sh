d828dee1bca8eefab0274b349c7783de70974820
dorgon.chang <dorgonman@hotmail.com>
2024-04-25 01:35:48 +0800

    Fix .nuget\install_game_package_from_local.sh

--- .nuget/install_game_package_from_local.sh
+++ .nuget/install_game_package_from_local.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-export FEED_NAME="https://nexus.horizonia.vpnplus.to/repository/nuget-hosted/"
+export FEED_NAME="http://nexus.horizonia.vpnplus.to/repository/nuget-hosted/"
 ./install_game_package.sh ${FEED_NAME}
