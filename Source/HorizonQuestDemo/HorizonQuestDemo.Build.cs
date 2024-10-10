faa84c8cb69ef899ea922140b7d04f873652cbcd
dorgon.chang <dorgonman@hotmail.com>
2024-10-10 09:51:07 +0800

    [Android] Update com.android.billingclient:billing:6.0.1

--- Source/HorizonQuestDemo/HorizonQuestDemo.Build.cs
+++ Source/HorizonQuestDemo/HorizonQuestDemo.Build.cs
@@ -1,5 +1,6 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
-
+using System;
+using System.IO;
 using UnrealBuildTool;
 
 public class HorizonQuestDemo : ModuleRules
@@ -11,5 +12,13 @@ public class HorizonQuestDemo : ModuleRules
 		PublicDependencyModuleNames.AddRange(new string[] { 
 			"Core", "CoreUObject", "Engine", "InputCore",
 			"HorizonQuest"});
+		string ProjectPath = Path.GetFullPath(Path.Combine(ModuleDirectory, "../../"));
+		if(Target.ProjectFile != null)
+		{
+			ProjectPath = Path.GetDirectoryName(Target.ProjectFile.ToString());
+
+		}
+         // https://docs.unrealengine.com/en-US/Platforms/Mobile/UnrealPluginLanguage/index.html
+        AdditionalPropertiesForReceipt.Add("AndroidPlugin", Path.Combine(ProjectPath, "Source", "Game_UPL.xml"));
 	}
 }
